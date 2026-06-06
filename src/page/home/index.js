b821533064de098f6b76fb0619bbbf33d4fb552a
list <dg2620628@daegun.hs.kr>
2026-06-06 22:13:27 +0900

    메인 홈페이지 헤더 수정

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -9,19 +9,29 @@ function HomePage() {
         <div className="HomePage">
             <header className='header'>
                 <Link to="/" className='logo-box'>
-                    <Logo className='logo'/>
+                    <span className='logo-icon'>
+                        <Logo className='logo'/>
+                    </span>
+                    <span>
+                        <div className='name'>출첵커</div>
+                        <div className='description'>출결관리 시스템</div>
+                    </span>
                 </Link>
                 <nav className='tabs'>
                     <Link to="/login" className='header-tab' id={1}>로그인</Link>
                     <Link to="/signup" className='header-tab' id={2}>회원가입</Link>
-                    <Link to="/login" className='header-tab' id={3}>후원하기</Link>
+                    <Link to="/introduction" className='header-tab' id={3}>기능소개</Link>
+                    <Link to="/ask" className='header-tab' id={4}>문의</Link>
                 </nav>
             </header>
             <main className='main'>
-                 김멀대TV
+                 [박스1]
             </main>
             <main className='main2'>
-                 김멀대TV
+                 [박스2]
+            </main>
+            <main className='main3'>
+                 [박스3]
             </main>
         </div>
     )
