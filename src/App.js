eda1dcdd8ba77ae12fc40036e6fb57e077717b15
정창진 <dg2510427@daegun.hs.kr>
2026-06-24 05:27:51 +0000

    initial clean commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+
+import LoginPage from './page/login';
+import SignupPage from './page/signup';
+import HomePage from './page/home';
+import SideBar from './components/SideBar';
+import HeaderBar from './components/HeaderBar';
+
+function App() {
+    let [status, setStatus] = useState(false);
+    const [user, setUser] = useState( localStorage.getItem('user_session') ?
+        JSON.parse(localStorage.getItem('user_session')) :
+        { 
+            name: null,
+            role: "Student",
+            email: null,
+            password: null,
+            isLogin: false
+        }
+    );
+    useEffect(() => {
+        localStorage.setItem('user_session', JSON.stringify(user));
+    }, [user]);
+    return (
+        <BrowserRouter>
+            <div className='app-container'>
+                <SideBar status={ status } setStatus={ setStatus } user={ user } setUser={ setUser }/>
+                <div className='content-container'>
+                    <HeaderBar status={ status } setStatus={ setStatus } user={ user } setUser={ setUser }/>
+                    <div className='main-container'>
+                        <Routes>
+                            <Route path="/" element={<HomePage/>}/>
+                            <Route path="/home" element={<HomePage/>}/>
+                            <Route path="/login" element={<LoginPage user={user} setUser={setUser}/>}/>
+                            <Route path="/signup" element={<SignupPage user={ user } setUser={ setUser }/>}/>
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App;
---
+++ src/page/login/index.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react'
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { ReactComponent as EyePasswordShow } from '../../assets/eye-password-show.svg';
+import { ReactComponent as EyePasswordHide } from '../../assets/eye-password-hide.svg';
+import './styles.css';
+
+import UserInput from '../../components/UserInput';
+import UserButton from '../../components/UserButton';
+import ToggleToken from '../../components/ToggleToken';
+import { Link, useNavigate, useOutletContext } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import UserManager from '../../server/utils/UserManager';
+
+function LoginPage({user, setUser}) {
+    const navigate = useNavigate();
+    async function redirect(user) {
+        let data = await UserManager.profile();
+        if(data.success && user.isLogin) {
+            return navigate('/home', { replace: true });
+        }
+    }
+    async function login(user) {
+        let data = await UserManager.logIn(user.name, user.password, user.role);
+        if(data.success) {
+            setUser({...user, isLogin: true});
+            alert("로그인 성공");
+        } else {
+            setUser({...user, isLogin: false});
+            alert("로그인  실패\n" + JSON.stringify(data));
+        }
+        return;
+    }
+    useEffect(() => {
+        redirect(user);
+    }, [user.isLogin]);
+
+    useEffect(() => {
+        redirect(user);
+    }, []);
+    
+    const setUserName = (event) => {
+        return setUser({...user, name : event.target.value });
+    };
+    const setUserRole = (event) => {
+        setRole(event.target.value == "학생" ? "Student" : "Teacher");
+        return setUser({...user, role : event.target.value === "학생" ? "Student" : "Teacher"});
+    };
+    const setUserEmail = (event) => {
+        return setUser({...user, email : event.target.value });
+    };
+    const setUserPW = (event) => {
+        return setUser({...user, password : event.target.value });
+    };
+
+    const [role, setRole] = useState('Student');
+
+
+
+    const [lockedVisible, setLockedVisible] = useState(false);
+    const [hoverVisible, setHoverVisible] = useState(false);
+
+    const isVisible = hoverVisible || lockedVisible;
+    
+
+    return (
+        <div className="LoginPage">
+            <Helmet>
+                <title>출첵커 | 로그인</title>
+            </Helmet>
+            <div className='login'>
+                <Link to="/">
+                    <Logo />
+                </Link>
+                <form>
+                    <div className='credentials-row toggle'>
+                        <ToggleToken
+                            name='user-type'
+                            value='학생'
+                            checked={role === 'Student'}
+                            onChange={setUserRole}
+                        />
+                        <ToggleToken
+                            name='user-type'
+                            value='교사'
+                            checked={role === 'Teacher'}
+                            onChange={setUserRole}
+                        />
+                        <div className='indicator'></div>
+                    </div>
+                    <div className='credentials-row'>
+                        <div className='inputs'>
+                            <UserInput
+                                name='user-name'
+                                type='text'
+                                placeholder='유저 아이디'
+                                value={user.name}
+                                onChange={ setUserName }
+                            />
+                            <div className='inputs pw-visible'>
+                                <UserInput
+                                    name='user-password'
+                                    type={ isVisible ? 'text' : 'password' }
+                                    placeholder='비밀번호'
+                                    value={user.password}
+                                    onChange={ setUserPW }
+                                />
+                                <div className='visible-toggle' 
+                                    onClick={() => { setLockedVisible(!lockedVisible) }} 
+                                    onMouseOver={() => { setHoverVisible(true) }} 
+                                    onMouseOut={() => { setHoverVisible(false) }}
+                                >
+                                    <span id='pw-visible-btn'> { isVisible ? <EyePasswordShow width="30px" height="14px"/> : <EyePasswordHide width="30px" height="14px"/> }</span>
+                                </div>
+                            </div>
+                        </div>
+                        <UserButton
+                            text='로그인'
+                            onClick={async () => { await login(user); }}
+                        />
+                    </div>
+                    <div className="direct-signup">
+                        계정이 없다면?&nbsp;&nbsp;<Link to="/signup"><span>가입하러 가기</span></Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
