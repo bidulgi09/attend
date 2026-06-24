85096d3dd9ee6dcfd0b843cfa642320a7aa2c01e
정창진 <dg2510427@daegun.hs.kr>
2026-06-24 03:06:07 +0000

    remove env from tracking

--- src/mysql_middleware/config/database.js
+++ src/mysql_middleware/config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "bidulgi",
-    database: "main"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "bidulgi",
+    database: process.env.DB_NAME || "main",
+    port: process.env.DB_PORT || 3306
 }
