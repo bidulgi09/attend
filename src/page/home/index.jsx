59b3f05333eed88a1e1308c9195fd8f42391050f
list <dg2620628@daegun.hs.kr>
2026-07-05 01:24:30 +0900

    home tab 수정

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,74 +1,9 @@
 import React from 'react';
-import './styles.css';
-import { Helmet } from 'react-helmet-async';
-import edit from '../../assets/edit.png';
-
-import banner from '../../assets/배너.png';
-
-import GhostBox from '../../components/GhostBox';
-import Schedule from '../../components/Schedule';
-import NotesTab from '../../components/NotesTab';
-import LogTab from '../../components/LogTab';
+import TeacherPage from './TeacherPage.jsx';
+import StudentPage from './StudentPage.jsx';
 
 function HomePage({ user, setUser }) {
-    let data = user ? user.data : [
-        ["국어", "수학", "영어", "과학", "사회"],
-        ["체육", "음악", "미술", "정보", "역사"],
-        ["국어", "수학", "영어", "과학", "사회"],
-        ["체육", "음악", "미술", "정보", "역사"],
-        ["국어", "수학", "영어", "과학", "사회"]
-    ];
-
-    const logData = [
-    { date: "2023-01-01", status: "출석", subject: "국어" },
-    { date: "2023-01-02", status: "출석", subject: "체육" },
-    { date: "2023-01-02", status: "출석", subject: "미술" },
-    { date: "2023-01-03", status: "출석", subject: "음악" },
-    { date: "2023-01-04", status: "결석", subject: "미술" },
-    { date: "2023-01-05", status: "조퇴", subject: "음악" },
-    { date: "2023-01-06", status: "결과", subject: "수학" },
-    { date: "2023-01-07", status: "출석", subject: "수학" },
-    { date: "2023-01-07", status: "출석", subject: "영어" },
-    { date: "2023-01-08", status: "결과", subject: "영어" },
-    { date: "2023-01-08", status: "출석", subject: "과학" },
-    { date: "2023-01-08", status: "출석", subject: "사회" },
-    { date: "2023-01-09", status: "결과", subject: "과학" },
-    { date: "2023-01-10", status: "결과", subject: "사회" },
-    { date: "2023-01-11", status: "결석", subject: "체육" },
-    { date: "2023-01-11", status: "출석", subject: "한문" },
-    { date: "2023-01-12", status: "출석", subject: "코딩" },
-    { date: "2023-01-13", status: "결과", subject: "국어" }
-];
-    return (
-        <div className="HomePage">
-            <Helmet>
-                <title>출첵커 | 홈</title>
-            </Helmet>
-            <div className='main'>
-                <NotesTab NotesData={{ attendance: 10, result: 5, absence: 2, earlyLeave: 1 }} />
-                <LogTab LogData={logData} />
-            </div>
-            <div className='profile-tab contents-wrapper'>
-                <GhostBox/>
-                <div className='profile-info-wrapper'>
-                    <div className='profile-img'>
-                        <img src={(user && user.avatar) ? user.avatar : 'https://i.pinimg.com/originals/78/20/45/7820459062e377482125ab4bafbd992f.jpg'} alt="Profile" />
-                    </div>
-                    <div className='name row-wrapper'>
-                        <p>{user ? user.name : 'Guest'}</p>
-                        <img className='edit-icon' src={edit} width='12vh' height='12vh' />
-                    </div>
-                    <div className='grade'>
-                        {user ? user.grade : 'N/A'}
-                    </div>
-                    <form className='attendence-form'>
-                        <input type="text" placeholder="출석 코드"></input>
-                    </form>
-                </div>
-                <Schedule scheduleData={data}/>
-            </div>
-        </div>
-    )
+    return (user && user.role) === "Student" ? StudentPage : TeacherPage;
 }
 
 export default HomePage;
