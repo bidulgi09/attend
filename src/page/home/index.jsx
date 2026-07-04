3e98359d099dc0a761971fcb06f8447056b5d8d1
list <dg2620628@daegun.hs.kr>
2026-07-05 01:36:00 +0900

    home tab 수정

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -3,7 +3,7 @@ import TeacherPage from './TeacherPage.jsx';
 import StudentPage from './StudentPage.jsx';
 
 function HomePage({ user, setUser }) {
-    return (user && user.role) === "Student" ? <StudentPage user={user} setUser={setUser}/> : <TeacherPage user={user} setUser={setUser}/>;
+    return (user && user.role === "Student") ? <StudentPage user={user} setUser={setUser}/> : <TeacherPage user={user} setUser={setUser}/>;
 }
 
 export default HomePage;
