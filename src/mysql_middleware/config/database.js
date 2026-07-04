6b8e1da6e87866cd8d8265c0c556ee84bbe5e53b
list <dg2620628@daegun.hs.kr>
2026-07-05 00:10:00 +0900

    test

--- src/mysql_middleware/config/database.js
+++ src/mysql_middleware/config/database.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' });
+dotenv.config();
 
 let dbconfig = {
     host: process.env.VITE_DB_HOST,
     user: process.env.VITE_DB_USER,
     password: process.env.VITE_DB_PASSWORD,
     database: process.env.VITE_DB_NAME,
-    port: process.env.VITE_DB_PORT,
+    port: parseInt(process.env.VITE_DB_PORT || '12749', 10),
     ssl: {
        rejectUnauthorized: false
     }
