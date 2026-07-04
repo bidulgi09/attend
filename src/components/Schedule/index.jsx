3f1a177635956560d532133109bebf58c90ad82b
list <dg2620628@daegun.hs.kr>
2026-07-05 00:31:21 +0900

    home tab 수정

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -21,7 +21,7 @@ function Schedule({ scheduleData }) {
                         for(let i = 0; i < scheduleData ? scheduleData.length : 7; i++) {
                             let row=[];
                             for(let j = 0; j < scheduleData ? scheduleData.length : 5; j++) {
-                                row.push(<td key={`td_${j}`}> scheduleData ? scheduleData[i][j] : "공강"}</td>);
+                                row.push(<td key={`td_${j}`}>{scheduleData ? scheduleData[i][j] : "공강"}</td>);
                             }
                             res.push(<tr key={`tr_${i}`}><th key={`th_${i}`}>{i+1}교시</th>{row}</tr>);
                         }
