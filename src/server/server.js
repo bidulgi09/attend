64898dd83714e7c645e84818924ed31f027a5a87
비둘기 <dg2620628@daegun.hs.kr>
2026-07-06 23:17:09 +0000

    file upload test

--- src/server/server.js
+++ src/server/server.js
@@ -39,6 +39,7 @@ app.get("/ping", (req, res) => {
 
 app.post('/upload', upload.single('file'), (req, res) => {
     if(!req.file) return res.status(500).json({ success: true, results: { isUploaded: false, reason: "Cannot find uploaded file."}});
+    console.log(req.file);
     if(!req.body.user || !req.body.user.role) return res.send({ success: true, results: { isUploaded: false, reason: "Unknown User."} }); 
     pool.getConnection(function(err, connection) {
         if(err) return res.sens({ success: true, results: { isUploaded: false, reason: err } });
