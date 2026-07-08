ef931faedf30bdd994b3e3e97b5d6a0d1cc47ad9
비둘기 <dg2620628@daegun.hs.kr>
2026-07-08 02:57:33 +0000

    getSubjectsInfo 추가

--- src/server/server.js
+++ src/server/server.js
@@ -410,7 +410,7 @@ app.post('/api/connectSubject', (req, res) => {
     })
 });
 
-app.get('/api/getSubjectsInfo', (req, res) => {
+app.get('/api/subjectList', (req, res) => {
     pool.getConnection(function(err, connection) {
         if(err) return res.status(500).json({ success: false, results: { isLoaded: false, reason: err }});
         connection.query(
