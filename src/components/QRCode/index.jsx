bd995ce316b6eefc18f58d85dcaa7da8ce60fbf2
비둘기 <dg2620628@daegun.hs.kr>
2026-07-09 04:43:05 +0000

    QR코드 로고 변경

--- src/components/QRCode/index.jsx
+++ src/components/QRCode/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QRCodeSVG } from 'qrcode.react';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo_final_final.png';
 import './styles.css';
 
 function QRCode({ url, iscreated, removelink }) {
