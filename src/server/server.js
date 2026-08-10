07dac9cfebc64b3b634e0dfdab513df52e3b676c
list <dg2620628@daegun.hs.kr>
2026-08-10 10:56:47 +0900

    회원가입 api 에러 수정 (JSON Stringify 이슈)

--- src/server/server.js
+++ src/server/server.js
@@ -127,7 +127,7 @@ app.post('/api/signUp', (req, res) => {
             })(), 
             req.body.email, 
             password_hash,
-            Array.from({ length: 7 }, () => Array.from({ length: 5 }, () => "공강"))
+            JSON.stringify(Array.from({ length: 7 }, () => Array.from({ length: 5 }, () => "공강")))
         ]; 
         connection.query(`INSERT INTO ${table} (id, name, email, password_hash, subjects) VALUES (?, ?, ?, ?, ?);`, datas, function(error, results, fields) { 
             connection.release();
