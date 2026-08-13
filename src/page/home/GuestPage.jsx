977dd3a018a92960bd59441b765921d077afe218
정창진 <dg2510427@daegun.hs.kr>
2026-08-13 02:34:12 +0000

    게스트 홈 페이지 수정:

--- src/page/home/GuestPage.jsx
+++ src/page/home/GuestPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './GuestStyles.css';
 
 function GuestPage() {
     return (
         <div className="GuestPage">
-
+            <div className="main-layout">
+                <div className="title-container">
+                    <div className="title">출첵커와 함께 하는 새로운 출석 관리 경험</div>
+                    <div className="description">효율적이고 간편한 출석 관리를 시작하세요!</div>
+                </div>
+                <div className="content-container">
+                    <img src="src/assets/서비스.png" width="200px" height="200px"></img>
+                    <div className="text">
+                        <div className="signin-btn">지금 회원가입하기</div>
+                        <div className="more-info">더 알아보기</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TeacherPage from './TeacherPage.jsx';
 import StudentPage from './StudentPage.jsx';
+import GuestPage from './GuestPage.jsx';
 
 function HomePage({ user, setUser }) {
     console.log(JSON.stringify(user, null, 4));
@@ -8,9 +9,7 @@ function HomePage({ user, setUser }) {
         <StudentPage user={user} setUser={setUser}/> : 
         (user && user.isLogin && user.role === "Teacher") ?
         <TeacherPage user={user} setUser={setUser}/> :
-        <div>
-            
-        </div>;
+        <GuestPage/>;
 }
 
 export default HomePage;
