4a2efb89946c9553c6a5e0ea71c4f1d75d866eb2
비둘기 <dg2620628@daegun.hs.kr>
2026-08-14 05:31:33 +0000

    icon add

--- src/page/home/GuestPage.jsx
+++ src/page/home/GuestPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './GuestStyles.css';
 import Image from '../../assets/main.png';
+import QuickAttend from '../../assets/quick_attend.svg?react';
 function GuestPage() {
     return (
         <div className="GuestPage">
@@ -15,6 +16,11 @@ function GuestPage() {
                     <nav className="text">
                         <Link to="/signup" className="signup-btn">지금 시작하기</Link>
                         <Link to="/more-info" className="more-info">더 알아보기</Link>
+                        <div className="quick-attend">
+                            <QuickAttend className="quick-attend-icon" width="100px" height="100px" color="white"/>
+                            <div className="quick-attend-text">빠른 출석 체크</div>
+                            <div className="quick-attend-subtext">간단한 QR/코드 기반 체크</div>
+                        </div>
                     </nav>
                 </div>
             </div>
