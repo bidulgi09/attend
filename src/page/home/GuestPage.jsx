9b9b737126fe88dbcee5464ca0ff5e92602db3fa
list <dg2620628@daegun.hs.kr>
2026-08-16 00:56:28 +0900

    아이콘 추가

--- src/page/home/GuestPage.jsx
+++ src/page/home/GuestPage.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './GuestStyles.css';
 import Image from '../../assets/main.png';
 import QuickAttend from '../../assets/quick_attend.svg?react';
+import NoDownload from '../../assets/no_need_download.svg?react';
 function GuestPage() {
     return (
         <div className="GuestPage">
@@ -16,10 +17,17 @@ function GuestPage() {
                     <nav className="text">
                         <Link to="/signup" className="signup-btn">지금 시작하기</Link>
                         <Link to="/more-info" className="more-info">더 알아보기</Link>
-                        <div className="quick-attend">
-                            <QuickAttend className="quick-attend-icon" width="100px" height="100px" color="white"/>
-                            <div className="quick-attend-text">빠른 출석 체크</div>
-                            <div className="quick-attend-subtext">간단한 QR/코드 기반 체크</div>
+                        <div className="icons">
+                            <div className="quick-attend">
+                                <QuickAttend className="quick-attend-icon" width="100px" height="100px" color="white"/>
+                                <div className="quick-attend-text">빠른 출석 체크</div>
+                                <div className="quick-attend-subtext">간단한 QR/코드 기반 체크</div>
+                            </div>
+                            <div className="no-download">
+                                <NoDownload className="no-download-icon" width="100px" height="100px" color="white"/>
+                                <div className="no-download-text">필요없는 앱 설치</div>
+                                <div className="no-download-subtext">웹 기반 출석체크</div>
+                            </div>
                         </div>
                     </nav>
                 </div>
