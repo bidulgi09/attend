bacec24f0b9cd89c6a916631d731cccb292b6a5f
list <dg2620628@daegun.hs.kr>
2026-08-17 22:02:27 +0900

    update

--- src/components/selectSubjectPopup/index.jsx
+++ src/components/selectSubjectPopup/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useImperativeHandle } from 'react';
 import './styles.css';
 
 import SubjectManager from '../../server/utils/SubjectManager.js';
