797e3f536fcc08c6cb9bb9386f281a0fe49d36ed
비둘기 <dg2620628@daegun.hs.kr>
2026-08-20 23:32:24 +0000

    asdf

--- src/page/home/StudentPage.jsx
+++ src/page/home/StudentPage.jsx
@@ -123,7 +123,7 @@ function StudentPage({ user, setUser }) {
                     </div>
                     <div className="student-list">
                         <div className="slide_box">
-                            <DailySchedule className="slide_item" scheduleData={user.subjects.map(v => v[currentDay].name || "공강")}/>
+                            <DailySchedule className="slide_item" scheduleData={user.subjects.map(v => v[currentDay] || {})}/>
                             <NotesTab className="slide_item" NotesData={{ attendance: 10, result: 5, absence: 2, earlyLeave: 1 }} />
                             <LogTab className="slide_item" LogData={logData} />
                         </div>
