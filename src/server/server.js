192416164b27d1b5c6aaa9de7b926cd075028fcb
비둘기 <dg2620628@daegun.hs.kr>
2026-09-03 05:52:04 +0000

    asdf

--- src/server/server.js
+++ src/server/server.js
@@ -534,7 +534,7 @@ app.post('/api/attendanceSession', authenticateToken, (req, res) => {
             }
             if(rows.length > 0) {
                 connection.release();
-                return res.json({ success: false, results: { isCreated: false, reason: "An active session already exists for this subject." }});
+                return res.json({ success: false, results: { isCreated: false, session_id: rows[0].id, token, code, expires_at, reason: "An active session already exists for this subject." }});
             }
             connection.query(`
             INSERT INTO attendance_sessions (subject_id, teacher_id, code, token, expires_at) 
