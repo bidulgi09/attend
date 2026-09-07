cf54798b428fc0bafd0675bb12f2defa412b8858
비둘기 <dg2620628@daegun.hs.kr>
2026-09-07 06:03:07 +0000

    attendanceSession req.user value test

--- src/server/server.js
+++ src/server/server.js
@@ -307,8 +307,6 @@ app.post('/api/updateUser', (req, res) => {
     })
 });
 app.post('/api/refresh', (req, res) => {
-    console.log("========== REFRESH ==========");
-    console.log("refresh_token 존재:", !!req.cookies.refresh_token);
     const REFRESH_TOKEN_EXPIRED_IN=(()=>new Date(Date.now() + 7*24*60*60*1000))();
     const ACCESS_TOKEN_EXPIRED_IN=(()=>new Date(Date.now() + 3*60*60*1000))();
     pool.getConnection(function(err, connection) {
@@ -510,6 +508,8 @@ app.get('/api/subjectList', (req, res) => {
 app.post('/api/attendanceSession', authenticateToken, (req, res) => {
     pool.getConnection(function(err, connection) {
         if(err) return res.status(500).json({ success: false, results: { isCreated: false, reason: err }});
+        
+        console.log("req.user: ", req.user);
         if(req.user.role !== "Teacher") {
             connection.release();
             res.send({ success: false, results: { isCreated: false, reason: "Unauthorized" } });
